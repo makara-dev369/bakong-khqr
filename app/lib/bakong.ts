88149e864f6a05ee322ae02ac983a64c48b2cc5d
Makara Dev369 <81961095+makara-dev369@users.noreply.github.com>
2026-06-05 16:51:33 +0700

    update

--- app/lib/bakong.ts
+++ app/lib/bakong.ts
@@ -9,7 +9,7 @@ const individualInfo = new IndividualInfo(
 );
 const BAKONG_API_URL = "https://api-bakong.nbc.gov.kh";
 const API_CHECK_PAYMENT_URL =  BAKONG_API_URL + "/v1/check_transaction_by_md5";
-const BAKONG_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiYjFkZjU2Mjg0OGMxNDUwOSJ9LCJpYXQiOjE3NzA4NjI2NDIsImV4cCI6MTc3ODYzODY0Mn0.4o2XqxTBN54lstJcxMkIeyiTtRdgzBTVMojlWpCFW_Y"
+const BAKONG_TOKEN = "_TOKEN"
 
 export { individualInfo, BakongKHQR ,API_CHECK_PAYMENT_URL,BAKONG_TOKEN};    
 
